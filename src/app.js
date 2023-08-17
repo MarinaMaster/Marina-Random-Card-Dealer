7002922691c153377dfb91ee903bdcdc3c5c87cb
Marina Master <marina777master@mail.ru>
2023-08-17 00:01:47 -0400

    complite card and timer

--- src/app.js
+++ src/app.js
@@ -7,5 +7,46 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //console.log("Hello Rigo from the console!");
+  //};
+  let images = document.querySelectorAll(".images");
+  let number = document.getElementById("number");
+
+  const button = document.querySelector(".button");
+  button.addEventListener("click", event => {
+    list();
+  });
+
+  function list() {
+    const suits = ["♦", "♥", "♠", "♣"];
+    const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+    const randomSuitIndex = Math.floor(Math.random() * suits.length);
+    for (const image of images) {
+      image.textContent = suits[randomSuitIndex];
+      image.style.color =
+        image.textContent === "♦" || image.textContent === "♥"
+          ? "red"
+          : "black";
+    }
+
+    const randomNumbersIndex = Math.floor(Math.random() * numbers.length);
+    number.textContent = numbers[randomNumbersIndex];
+  }
+  let timer;
+  let startTime = document.querySelector(".timer");
+  (function() {
+    let seconds = 1;
+    timer = setInterval(() => {
+      startTime.innerHTML = "00:" + seconds;
+      seconds++;
+    }, 1000);
+  })();
+
+  const clickOnTime = setInterval(() => {
+    list();
+  }, 10000);
+
+  function weightConverter(valNum) {
+    document.getElementById("inputWeight").innerHTML = valNum;
+  }
 };
